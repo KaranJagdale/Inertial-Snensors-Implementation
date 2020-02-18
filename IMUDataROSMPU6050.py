08f14ffb0c992f6bb274a2dcb36d1fd5d665faea
KaranJagdale <37465796+KaranJagdale@users.noreply.github.com>
2020-02-18 12:47:35 +0530

    Create IMUDataROSMPU6050.py

---
+++ IMUDataROSMPU6050.py
@@ -0,0 +1,85 @@
+import rospy
+from fb5_torque_ctrl.msg import IMUmsg
+import smbus			#import SMBus module of I2C
+from time import sleep          #import
+#define msg in cmakelist
+
+#some MPU6050 Registers and their Address
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
+
+def MPU_Init():
+	#write to sample rate register
+	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+	
+	#Write to power management register
+	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+	
+	#Write to Configuration register
+	bus.write_byte_data(Device_Address, CONFIG, 0)
+	
+	#Write to Gyro configuration register
+	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+	
+	#Write to interrupt enable register
+	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+
+def read_raw_data(addr):
+	#Accelero and Gyro value are 16-bit
+        high = bus.read_byte_data(Device_Address, addr)
+        low = bus.read_byte_data(Device_Address, addr+1)
+    
+        #concatenate higher and lower value
+        value = ((high << 8) | low)
+        
+        #to get signed value from mpu6050
+        if(value > 32768):
+                value = value - 65536
+        return value
+
+
+bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+Device_Address = 0x68   # MPU6050 device address
+
+MPU_Init()
+
+#print (" Reading Data of Gyroscope and Accelerometer")
+def pub():
+  rospy.init_node('pubIMU',anonymous = True)
+  pubIMU = rospy.Publisher('IMU messege',IMUmsg,queue_size = 10)
+  msg = IMUmsg()
+  rate = rospy.Rate(0.5)
+  while not rospy.is_shutdown():
+
+    #Read Accelerometer raw value
+    acc_x = read_raw_data(ACCEL_XOUT_H)
+    acc_y = read_raw_data(ACCEL_YOUT_H)
+    acc_z = read_raw_data(ACCEL_ZOUT_H)
+
+    #Read Gyroscope raw value
+    gyro_x = read_raw_data(GYRO_XOUT_H)
+    gyro_y = read_raw_data(GYRO_YOUT_H)
+    gyro_z = read_raw_data(GYRO_ZOUT_H)
+
+    #Full scale range +/- 250 degree/C as per sensitivity scale factor
+    msg.Ax = acc_x/16384.0
+    msg.Ay = acc_y/16384.0
+    msg.Az = acc_z/16384.0
+
+    msg.Gx = gyro_x/131.0
+    msg.Gy = gyro_y/131.0
+    msg.Gz = gyro_z/131.0
+    print(msg)
+    pubIMU.publish(msg)
+    
+
+    rate.sleep()
